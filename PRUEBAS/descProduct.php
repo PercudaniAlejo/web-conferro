3e8019a7bcca30fce313445e5cfdaf71d58437d7
Alejo Percudani <alejopercudani@outlook.com>
2020-12-06 14:53:35 -0300

    categorias - main terminada

---
+++ PRUEBAS/descProduct.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PRODUCT DESCRIPTION</title>
+
+	<!-- CSS CUSTOM -->
+	<link rel="stylesheet" href="descProduct.css">
+	<link rel="stylesheet" href="../css/header-reponsive">
+	<link rel="stylesheet" href="../footer/footer.css">
+	<link rel="stylesheet" href="../header/header.css">
+
+	<!-- FOOTER -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<!-- -------------------------- -->
+    	    <!-- NAVEGACIÓN -->
+	<!-- -------------------------- -->
+ 	<div class="topnav" id="myTopnav">
+ 		<img src="../img/conferro.jpg" alt="">  
+  		<a href="../index.php">INICIO</a>
+  		<a href="../html/categories.php">PRODUCTOS</a>
+  		<a href="#contact">CONTACTO</a>
+  		<a href="javascript:void(0);" class="icon" onclick="myFunction()" id="btn-option">
+    		<i class="fa fa-bars"></i>
+  		</a>
+	</div>
+
+
+  
+
+  <div class="container bootdey">
+<div class="col-md-12">
+<section class="panel">
+      <div class="panel-body">
+          <div class="col-md-6">
+              <div class="pro-img-details">
+                  <img src="https://via.placeholder.com/550x380/FFB6C1/000000" alt="">
+              </div>
+              <div class="pro-img-list">
+                  <a href="#">
+                      <img src="https://via.placeholder.com/115x100/87CEFA/000000" alt="">
+                  </a>
+                  <a href="#">
+                      <img src="https://via.placeholder.com/115x100/FF7F50/000000" alt="">
+                  </a>
+                  <a href="#">
+                      <img src="https://via.placeholder.com/115x100/20B2AA/000000" alt="">
+                  </a>
+                  <a href="#">
+                      <img src="https://via.placeholder.com/120x100/20B2AA/000000" alt="">
+                  </a>
+              </div>
+          </div>
+          <div class="col-md-6">
+              <h4 class="pro-d-title">
+                  <a href="#" class="">
+                      Leopard Shirt Dress
+                  </a>
+              </h4>
+              <p>
+                  Praesent ac condimentum felis. Nulla at nisl orci, at dignissim dolor, The best product descriptions address your ideal buyer directly and personally. The best product descriptions address your ideal buyer directly and personally.
+              </p>
+              <div class="product_meta">
+                  <span class="posted_in"> <strong>Categories:</strong> <a rel="tag" href="#">Jackets</a>, <a rel="tag" href="#">Men</a>, <a rel="tag" href="#">Shirts</a>, <a rel="tag" href="#">T-shirt</a>.</span>
+                  <span class="tagged_as"><strong>Tags:</strong> <a rel="tag" href="#">mens</a>, <a rel="tag" href="#">womens</a>.</span>
+              </div>
+              <div class="m-bot15"> <strong>Price : </strong> <span class="amount-old">$544</span>  <span class="pro-price"> $300.00</span></div>
+              <div class="form-group">
+                  <label>Quantity</label>
+                  <input type="quantiy" placeholder="1" class="form-control quantity">
+              </div>
+              <p>
+                  <button class="btn btn-round btn-danger" type="button"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+              </p>
+          </div>
+      </div>
+  </section>
+  </div>
+  </div>
+
+  
+
+  	<!-- -------------------------- -->
+             <!-- FOOTER -->
+	<!-- -------------------------- -->
+
+    <?php
+      include ('../footer/footer.php');
+    ?>
+</body>
+</html>
