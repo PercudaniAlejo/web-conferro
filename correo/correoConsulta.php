4ec9f7f2cd68cdb70e01a0111d340d21b4c0464b
Alejo Percudani <alejopercudani@outlook.com>
2021-01-07 11:50:32 -0300

    Envia email, pero no en local (000web)

--- correo/correoConsulta.php
+++ correo/correoConsulta.php
@@ -1,17 +1,17 @@
 <?php
 	
 	if (isset($_POST['btnEnviar'])) {
-		$nombre=$_POST['name'];
-        $correo=$_POST['email'];
+   		$enviar = "percudanialejo@gmail.com";
 		$asunto =$_POST['numProd'];
         $mensaje=$_POST['msg'];
-   		$enviar = 'percudanialejo@gmail.com';
-		$header = 'From: ' . $correo;
-		$msjCorreo = "Nombre: $nombre\n E-Mail: $correo\n ID. Producto: $asunto\n Mensaje:\n $mensaje";
-   		if (@mail($enviar, $asunto, $mensaje, $header, $msjCorreo)) {
+        
+		$nombre=$_POST['name'];
+        $correo=$_POST['email'];
+		$msjCorreo = "Nombre: $nombre\n E-Mail: $correo\n ID. Producto: $asunto\n Mensaje:\n\n $mensaje";
+   		if (@mail($enviar, $asunto, $msjCorreo)) {
 	    	echo "<h4> Mensaje enviado. </h4>";
 		} else {
-		  	echo "<h4> No se puedo enviar el mensaje. </h4>";
+		  	echo "<h4> No se puesdado enviar el mensaje. </h4>";
 		}
 	}
 ?>
